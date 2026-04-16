70f1dcb6c2e077b582171be2b1e4c19327bd6a79
Link-Games <benjamin.zdunich@outlook.com>
2026-04-16 15:18:31 -0400

    Added functions to change cursor size

--- src/cursor.c
+++ src/cursor.c
@@ -16,3 +16,19 @@ void set_cursor(int x, int y){
 	outb(VGA_PORT_INDEX, 0x0F);
 	outb(VGA_PORT_DATA, (uint8_t)(position & 0xFF));
 }
+
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
+}
+
+
+void disable_cursor()
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, 0x20);
+}
--- src/cursor.h
+++ src/cursor.h
@@ -5,4 +5,12 @@
 
 void set_cursor(uint16_t x, uint16_t y);
 
-#endif //SERIAL_H
+//Define cursor character
+//cursor_start: scanline that cursor starts, RANGE 0-15; cursor_end: scanline that cursor starts, RANGE 0-15;
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end);
+
+//Disable cursor
+//NULL
+void disable_cursor();
+
+#endif //CURSOR_H
